63126ff283c27e4b7c1699f8c5dd3800ee0455c5
AnnamTRAN <annampierretran@gmail.com>
2025-05-21 21:29:12 +0200

    🚧 - WORKING ON creating JWT class

--- shared/events/client-to-server.ts
+++ shared/events/client-to-server.ts
@@ -1,5 +1,5 @@
 //! Input events into ./README.md
-import { ResponseCallback } from "../response/callback";
+import type { ResponseCallback } from "../response/callback";
 
 export type ClientsToServerEvents = {
     "message": (payload: string, callback: ResponseCallback<null>) => void;
